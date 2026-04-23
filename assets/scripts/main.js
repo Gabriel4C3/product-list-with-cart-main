e535175c06244a7161f3c6763bc0483938e12297
Gabriel <gabrielcarvalho20111@gmail.com>
2026-04-22 22:35:06 -0300

    adicionando resposividade as imagens

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,7 +6,11 @@ fetch('data.json')
       const productElement = document.createElement('section');
         productElement.classList.add('product');
         productElement.innerHTML = `
-          <img src="${product.image.desktop}" alt="${product.name}">
+          <picture>
+            <source media="(min-width: 768px)" srcset="${product.image.tablet}">
+            <source media="(min-width: 375px)" srcset="${product.image.mobile}">
+            <img src="${product.image.desktop}" alt="${product.name}">
+          </picture>
           <h2>${product.name}</h2>
           <p class="price">$${product.price.toFixed(2)}</p>
           <button class="add-to-cart">Add to Cart</button>
