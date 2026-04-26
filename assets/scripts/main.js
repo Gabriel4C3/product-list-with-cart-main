c826337dcc207179611b9f0a5bf55af19060a843
Gabriel <gabrielcarvalho20111@gmail.com>
2026-04-25 23:07:24 -0300

    adção da funcionalidade no botão "Add to Cart" e ajustes no layout para acomodar o botão dentro da caixa do produto.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,13 +10,44 @@ fetch('data.json')
             <source media="(min-width: 768px)" srcset="${product.image.tablet}">
             <source media="(min-width: 375px)" srcset="${product.image.mobile}">
             <img class="product-image" src="${product.image.desktop}" alt="${product.name}">
+           <div class= "cart-box">
             <button class="add-to-cart"> Add to Cart</button>
+           </div>
+            
           </picture>
           <p class="category">${product.category}</p>
           <h2>${product.name}</h2>
-          <p class="price">$${product.price.toFixed(2)}</p>
-        
-        `;
+          <p class="price">$${product.price.toFixed(2)}</p>`;
+          const cartbox = productElement.querySelector('.cart-box');
+          let quantity = 0;
+          function renderizar(){
+          if ( quantity ===0) {
+            cartbox.innerHTML = `<button class="add-to-cart"> Add to Cart</button>`;
+            const addButton = cartbox.querySelector('.add-to-cart');
+            addButton.addEventListener('click', () => {
+              quantity++;
+              renderizar();
+            });
+          } else {
+            cartbox.innerHTML = `
+            <div class="quantity-controls">
+              <button class="decrease">-</button>
+              <span class="quantity">${quantity}</span>
+              <button class="increase">+</button>
+            </div>`;
+            
+            const decreaseButton = cartbox.querySelector('.decrease');
+            const increaseButton = cartbox.querySelector('.increase');
+            decreaseButton.addEventListener('click', () => {
+              quantity--;
+              renderizar();
+            });
+            increaseButton.addEventListener('click', () => {
+              quantity++;
+              renderizar();
+            });
+          }}
+          renderizar();
         main.appendChild(productElement);
       });
     });
